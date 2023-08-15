e3dd1718f9e02d3bf8facf6d36251be63b99ba11
Noam Milner <milner.noam@gmail.com>
2023-08-15 20:51:50 +0300

    Added solutions

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+
+sudo apt-get update && sudo apt-get install -y sshpass
+cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+sudo sed -i '/#PasswordAuthentication yes/c\PasswordAuthentication no' /etc/ssh/sshd_config
+sudo service ssh restart
+
